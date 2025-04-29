861aa1e097b0b6737f48bcebe1ded3facf32fa59
Jhon Pacalagua <jhonpacalagua@cbit-online.com>
2025-04-29 12:52:12 -0500

    outputs

--- main.tf
+++ main.tf
@@ -76,3 +76,16 @@ resource "aws_security_group" "nginx-server-sg" {
       Project = "webinar"
     }
 }
+
+##OUTPUTS
+output "server_public_ip" {
+  description = "Dirección IP pública de la instancia EC2"
+  value = aws_instance.nginx-server.public_ip
+  
+}
+
+output "server_public_dns" {
+  description = "Nombre DNS público de la instancia EC2"
+  value = aws_instance.nginx-server.public_dns
+  
+}
